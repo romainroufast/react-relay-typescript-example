36b4cb396c2ad6b8d0a47199a4e7c5c7f879b179
romain <romain.roufast@cedemo.com>
2019-11-05 13:19:08 +0100

    updated readme + relay watch as command param instead of config

--- relay.config.js
+++ relay.config.js
@@ -3,6 +3,5 @@ module.exports = {
     // Configuration options accepted by the `relay-compiler` command-line tool and `babel-plugin-relay`.
     src: "./src",
     schema: "./schema.graphql",
-    language: "typescript",
-    watch: true
+    language: "typescript"
 }
